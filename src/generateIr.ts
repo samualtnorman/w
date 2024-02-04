97af63017944fd086e3cc5eac1ba7a92a8463ea8
Samual <me@samual.uk>
2024-02-04 23:32:18 +0000

    replace `ExpressionIr[]` in `ExpressionIr` union with explicit `Block` object

--- src/generateIr.ts
+++ src/generateIr.ts
@@ -7,8 +7,8 @@ export type TypeIr = I32TypeIr | I64TypeIr
 export type I32LiteralIr = { tag: "I32Literal", value: number }
 export type GetLocalIr = { tag: "GetLocal", index: number }
 export type SetLocalIr = { tag: "SetLocal", index: number, value: ExpressionIr }
-export type CallIr = { tag: "CallIr", name: string, arguments: ExpressionIr }
-export type ExpressionIr = I32LiteralIr | GetLocalIr | CallIr | SetLocalIr | ExpressionIr[]
+export type BlockIr = { tag: "Block", children: ExpressionIr[], name: string | undefined }
+export type ExpressionIr = I32LiteralIr | GetLocalIr | SetLocalIr | BlockIr
 
 export type IrFunction = {
 	name: string
@@ -25,56 +25,50 @@ export const I32TypeIr: I32TypeIr = { tag: "I32Type" }
 export const I32LiteralIr = (value: number): I32LiteralIr => ({ tag: "I32Literal", value })
 export const GetLocalIr = (index: number): GetLocalIr => ({ tag: "GetLocal", index })
 export const SetLocalIr = (index: number, value: ExpressionIr): SetLocalIr => ({ tag: "SetLocal", index, value })
-
-export const CallIr = (name: string, arguments_: ExpressionIr): CallIr =>
-	({ tag: "CallIr", name, arguments: arguments_ })
+export const BlockIr = (children: ExpressionIr[], name?: string): BlockIr => ({ tag: `Block`, children, name })
 
 export function generateIr(downLeveledExpression: AbstractionExpression<TypeAnnotation>): IrModule {
 	unify(downLeveledExpression.type, FunctionType(IntType, IntType))
 
-	const irModule: IrFunction[] = []
 	const locals: TypeIr[] = []
-
 	const body = generateExpressionIr(downLeveledExpression.body, { [downLeveledExpression.argumentName]: 0 })
 
-	const functionIr: IrFunction = {
-		name: "main",
-		argumentTypes: undefined,
-		body,
-		export: true,
-		locals,
-		returnType: I32TypeIr
-	}
-
-	irModule.push(functionIr)
-
-	return irModule
+	return [
+		{
+			name: "main",
+			argumentTypes: [ I32TypeIr ],
+			body: BlockIr(body),
+			export: true,
+			locals,
+			returnType: I32TypeIr
+		}
+	]
 
 	function generateExpressionIr(
 		expression: AnnotatedExression,
 		nameToIndexes: Record<string, number>
-	): ExpressionIr {
+	): ExpressionIr[] {
 		switch (expression.tag) {
 			case ExpressionTag.False:
-				return I32LiteralIr(0)
+				return [ I32LiteralIr(0) ]
 
 			case ExpressionTag.True:
-				return I32LiteralIr(1)
+				return [ I32LiteralIr(1) ]
 
 			case ExpressionTag.Integer:
-				return I32LiteralIr(expression.value)
+				return [ I32LiteralIr(expression.value) ]
 
 			case ExpressionTag.Let: {
 				locals.push(I32TypeIr)
 
 				return [
-					SetLocalIr(locals.length, generateExpressionIr(expression.value, nameToIndexes)),
-					generateExpressionIr(expression.body, { ...nameToIndexes, [expression.name]: locals.length })
+					SetLocalIr(locals.length, BlockIr(generateExpressionIr(expression.value, nameToIndexes))),
+					...generateExpressionIr(expression.body, { ...nameToIndexes, [expression.name]: locals.length })
 				]
 			}
 
 			case ExpressionTag.Identifier: {
-				return GetLocalIr(nameToIndexes[expression.name]!)
+				return [ GetLocalIr(nameToIndexes[expression.name]!) ]
 			}
 
 			default:
