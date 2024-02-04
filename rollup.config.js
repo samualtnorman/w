37d4e51c3a06f8180fcd6047bf4e1b02bbeeb9e8
Samual <me@samual.uk>
2024-02-04 23:32:53 +0000

    add config stuff for building

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+#!rollup --config
+import babelPresetTypescript from "@babel/preset-typescript"
+import { babel } from "@rollup/plugin-babel"
+import { nodeResolve } from "@rollup/plugin-node-resolve"
+import { findFiles } from "@samual/lib/findFiles"
+import packageJson from "./package.json" assert { type: "json" }
+
+/** @typedef {import("rollup").RollupOptions} RollupOptions */
+
+const SOURCE_FOLDER = "src"
+
+/** @type {RollupOptions} */ export default {
+	external: Object.keys(packageJson.dependencies).map(name => new RegExp(`^${name}(?:$|/)`)),
+	input: Object.fromEntries(
+		(await findFiles(SOURCE_FOLDER)).filter(path => path.endsWith(".ts") && !path.endsWith(".d.ts"))
+			.map(path => [ path.slice(SOURCE_FOLDER.length + 1, -3), path ])
+	),
+	output: { dir: "dist" },
+	plugins: [
+		nodeResolve({ extensions: [ ".ts" ] }),
+		babel({ babelHelpers: "bundled", extensions: [ ".ts" ], presets: [ babelPresetTypescript ] })
+	],
+	strictDeprecations: true,
+	treeshake: { moduleSideEffects: false }
+}
