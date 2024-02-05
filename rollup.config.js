80f1604c5de4d9b3133e85a444d1a524ace0dda7
Samual Norman <me@samual.uk>
2024-02-05 12:39:24 +0000

    add sourcemaps

--- rollup.config.js
+++ rollup.config.js
@@ -15,7 +15,11 @@ const SOURCE_FOLDER = "src"
 		(await findFiles(SOURCE_FOLDER)).filter(path => path.endsWith(".ts") && !path.endsWith(".d.ts"))
 			.map(path => [ path.slice(SOURCE_FOLDER.length + 1, -3), path ])
 	),
-	output: { dir: "dist" },
+	output: {
+		dir: "dist",
+		sourcemap: true,
+		sourcemapPathTransform: relativeSourcePath => relativeSourcePath.slice(2)
+	},
 	plugins: [
 		nodeResolve({ extensions: [ ".ts" ] }),
 		babel({ babelHelpers: "bundled", extensions: [ ".ts" ], presets: [ babelPresetTypescript ] })
