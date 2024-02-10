133c6803c803077cfe899bfa6b1ff305f7982036
Samual <me@samual.uk>
2024-02-10 16:18:03 +0000

    add `expressionApplySubstitution()`

---
+++ src/expressionApplySubstitution.ts
@@ -0,0 +1,68 @@
+import { TypeTag, type BoolType, type FunctionType, type IntType, type Substitution } from "./Type"
+import type { TypedExpression } from "./inferTypes"
+import { typeApplySubstitution } from "./inferTypes/typeApplySubstitution"
+import { Expression, ExpressionTag } from "./parse"
+import { assert } from "@samual/lib/assert"
+
+export function expressionApplySubstitution(expression: TypedExpression, substitution: Substitution)
+: Expression<{ type: BoolType | IntType | FunctionType }> {
+	const type = typeApplySubstitution(expression.type, substitution)
+
+	assert(type.tag != TypeTag.TypeVariable, HERE)
+
+	switch (expression.tag) {
+		case ExpressionTag.RecursiveLet: {
+			return {
+				...expression,
+				value: expressionApplySubstitution(expression.value, substitution),
+				body: expressionApplySubstitution(expression.body, substitution),
+				type
+			}
+		}
+
+		case ExpressionTag.Abstraction: {
+			return {
+				...expression,
+				body: expressionApplySubstitution(expression.body, substitution),
+				type
+			}
+		}
+
+		case ExpressionTag.IfElse: {
+			return {
+				...expression,
+				condition: expressionApplySubstitution(expression.condition, substitution),
+				then: expressionApplySubstitution(expression.then, substitution),
+				else: expressionApplySubstitution(expression.else, substitution),
+				type
+			}
+		}
+
+		case ExpressionTag.LessThan:
+		case ExpressionTag.Add:
+		case ExpressionTag.Minus: {
+			return {
+				...expression,
+				left: expressionApplySubstitution(expression.left, substitution),
+				right: expressionApplySubstitution(expression.right, substitution),
+				type
+			}
+		}
+
+		case ExpressionTag.Identifier:
+		case ExpressionTag.Integer:
+			return { ...expression, type }
+
+		case ExpressionTag.Application: {
+			return {
+				...expression,
+				callee: expressionApplySubstitution(expression.callee, substitution),
+				argument: expressionApplySubstitution(expression.argument, substitution),
+				type
+			}
+		}
+
+		default:
+			throw Error(`Unhandled expression ${ExpressionTag[expression.tag]}`)
+	}
+}
--- src/playground.ts
+++ src/playground.ts
@@ -1,13 +1,8 @@
-import { inspect } from "util"
-import { FunctionType, IntType, TypeSchemeType, type Type } from "./Type"
-import { downLevel } from "./downLevel"
-import { generateBinaryenModule } from "./generateBinaryenModule"
-import { generateIr } from "./generateIr"
+import { FunctionType, IntType, TypeSchemeType } from "./Type"
+import { expressionApplySubstitution } from "./expressionApplySubstitution"
 import { inferTypes } from "./inferTypes"
-import { expressionToSource, expressionToString, parse, type AbstractionExpression } from "./parse"
-import { substitutionToString } from "./substitutionToString"
+import { expressionToString, parse } from "./parse"
 import { tokenise } from "./tokenise"
-import { typeToString } from "./typeToString"
 
 try {
 	const source = `
@@ -23,50 +18,56 @@ try {
 	const tokens = [ ...tokenise(source) ]
 	const ast = parse(tokens)
 
-	console.log(`Formatted Source:`)
-	console.log(expressionToSource(ast))
-	console.log()
+	// console.log(`Formatted Source:`)
+	// console.log(expressionToSource(ast))
+	// console.log()
 
 	const { substitution, ...typedAst } = inferTypes(ast, {
 		add: TypeSchemeType([], FunctionType(IntType, FunctionType(IntType, IntType)))
 	})
 
-	console.log(`Typed AST:`)
-	console.log(expressionToString(typedAst))
+	// console.log(`Typed AST:`)
+	// console.log(expressionToString(typedAst))
+	// console.log()
+
+	// console.log("Substitution:")
+	// console.log(substitutionToString(substitution))
+	// console.log()
+
+	const fullyTypedAst = expressionApplySubstitution(typedAst, substitution)
+
+	console.log("Fully Typed AST:")
+	console.log(expressionToString(fullyTypedAst))
 	console.log()
 
-	console.log("Substitution:")
-	console.log(substitutionToString(substitution))
-	console.log()
+	// console.log("Type:")
+	// console.log(typeToString(typedAst.type))
+	// console.log()
 
-	console.log("Type:")
-	console.log(typeToString(typedAst.type))
-	console.log()
+	// const downLeveledAst = downLevel(typedAst)
 
-	const downLeveledAst = downLevel(typedAst)
+	// console.log("Downleveled Source:")
+	// console.log(expressionToSource(downLeveledAst))
+	// console.log()
 
-	console.log("Downleveled Source:")
-	console.log(expressionToSource(downLeveledAst))
-	console.log()
+	// const irModule = generateIr(downLeveledAst as AbstractionExpression<{ type: Type }>)
 
-	const irModule = generateIr(downLeveledAst as AbstractionExpression<{ type: Type }>)
+	// console.log("IR:")
+	// console.log(inspect(irModule, { depth: Infinity, colors: true }))
+	// console.log()
 
-	console.log("IR:")
-	console.log(inspect(irModule, { depth: Infinity, colors: true }))
-	console.log()
+	// const binaryenModule = generateBinaryenModule(irModule)
+	// const wat = binaryenModule.emitStackIR()
 
-	const binaryenModule = generateBinaryenModule(irModule)
-	const wat = binaryenModule.emitStackIR()
+	// console.log("WAT:")
+	// console.log(wat)
+	// console.log()
 
-	console.log("WAT:")
-	console.log(wat)
-	console.log()
+	// const byteCode = binaryenModule.emitBinary()
+	// const { instance: wasmInstance } = await WebAssembly.instantiate(byteCode)
 
-	const byteCode = binaryenModule.emitBinary()
-	const { instance: wasmInstance } = await WebAssembly.instantiate(byteCode)
-
-	console.log("Result:")
-	console.log((wasmInstance.exports!.main as Function)(42))
+	// console.log("Result:")
+	// console.log((wasmInstance.exports!.main as Function)(42))
 } catch (error) {
 	console.error("Caught", (error as Record<string, unknown>).stack)
 }
